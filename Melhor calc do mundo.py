fd9e7c263f9a543404e5296ff3b42407b8b351a7
Akinori <126922127+GhostFacings1171@users.noreply.github.com>
2023-08-01 19:41:56 -0300

    Add files via upload

---
+++ Melhor calc do mundo.py
@@ -0,0 +1,8 @@
+
+numero1 = input("me fale um número:")
+numero2 = input("me fale outro número:")
+resultado = (numero1 + numero2)
+print(resultado)
+input("parabéns você hackeou a NASA")
+
+input("pressione Enter para sair")
