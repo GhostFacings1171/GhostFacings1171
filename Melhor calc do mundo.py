b3125a219922e7c3df942de59b6643c1bc74097a
Akinori <126922127+GhostFacings1171@users.noreply.github.com>
2023-08-03 22:08:11 -0300

    Delete Melhor calc do mundo.py

--- Melhor calc do mundo.py
+++
@@ -1,8 +0,0 @@
-
-numero1 = input("me fale um número:")
-numero2 = input("me fale outro número:")
-resultado = (numero1 + numero2)
-print(resultado)
-input("parabéns você hackeou a NASA")
-
-input("pressione Enter para sair")
